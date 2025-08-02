4479267e0a5f10bfcce454fb3f1e8b39f3dfefa1
2894638479 <2894638479@qq.com>
2025-08-02 19:25:38 +0800

    修复mixin兼容性

--- src/client/java/name/bluearchivehalo/mixin/FuckMojangWorldRendererDoubleRenderBug.java
+++ src/client/java/name/bluearchivehalo/mixin/FuckMojangWorldRendererDoubleRenderBug.java
@@ -10,9 +10,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 
-@Mixin(value = WorldRenderer.class,priority = 1)
+@Mixin(value = WorldRenderer.class,priority = 10000)
 public class FuckMojangWorldRendererDoubleRenderBug {
-    @Redirect(method = "render",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/ChunkBuilder$ChunkData;getBlockEntities()Ljava/util/List;"))
+    @Redirect(method = "render",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/ChunkBuilder$ChunkData;getBlockEntities()Ljava/util/List;"),require = 0)
     List<BlockEntity> fuckDoubleRender(ChunkBuilder.ChunkData instance){
         List<BlockEntity> list = instance.getBlockEntities();
         if(!list.isEmpty()){
