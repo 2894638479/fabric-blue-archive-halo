84f3fad4143277592997585bc1c3a71758df5c88
2894638479 <2894638479@qq.com>
2025-08-18 21:01:23 +0800

    支持等级衰减入口
    (cherry picked from commit 5669971de61b3c489ff501bc6e4a2e2d0abc1fc4)

--- src/client/kotlin/name/bluearchivehalo/BlueArchiveHaloClient.kt
+++ src/client/kotlin/name/bluearchivehalo/BlueArchiveHaloClient.kt
@@ -2,17 +2,23 @@ package name.bluearchivehalo
 
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.impl.client.rendering.BlockEntityRendererRegistryImpl
+import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.block.entity.BeaconBlockEntity
 import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.util.Identifier
 
 
 object BlueArchiveHaloClient : ClientModInitializer {
 	val id = "blue-archive-halo"
+    var shrinker:((BeaconBlockEntity) -> Int)? = null
+        private set
 	override fun onInitializeClient() {
 		BlockEntityRendererRegistryImpl.register(
 			BlockEntityType.BEACON,
 			::BeaconHaloRenderer
 		)
+        val shrinkers = FabricLoader.getInstance().getEntrypoints("blue_archive_halo_beacon_level_shrinker",Function1::class.java)
+        shrinkers.firstOrNull()?.let { shrinker = it as ((BeaconBlockEntity) -> Int) }
 	}
 	val texture = Identifier.of(id,"textures/pure_white.png")
 }
