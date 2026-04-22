bc86b5f4f25c867fbf7f8ab45f2f514d9fa62aa2
2894638479 <2894638479@qq.com>
2026-04-22 21:18:10 +0800

    修复光环残留

--- src/client/kotlin/io/github/u2894638479/bahalo/Entry.kt
+++ src/client/kotlin/io/github/u2894638479/bahalo/Entry.kt
@@ -60,7 +60,9 @@ class Entry: DslEntryService, ModMenuApi {
                         if(!loaded) return@removeIf false
                         val beacon = world.getBlockEntity(pos) as? BeaconBlockEntity ?: return@removeIf true
                         if(beacon.level == 0) {
-                            beacon.level = BeaconBlockEntity.updateLevel(world,beacon.pos.x,beacon.pos.y,beacon.pos.z)
+                            val level = BeaconBlockEntity.updateLevel(world,beacon.pos.x,beacon.pos.y,beacon.pos.z)
+                            beacon.level = level
+                            if(level == 0) return@removeIf true
                         }
                         false
                     }
